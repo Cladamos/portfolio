c79ec88f928d79372f0275ea21b4dc226aa6e24c
cladamos <caliskangokberk@gmail.com>
2026-04-17 22:42:09 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <p className="text-3xl font-bold underline bg-red-300">Hello World</p>
+    </div>
+  )
+}
+
+export default App
